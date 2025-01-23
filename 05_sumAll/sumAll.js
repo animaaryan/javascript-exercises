f8d724ef839ef7a5f120cef89b90c6a146fd7fce
animaaryan <aaryan.junnarkar@gmail.com>
2025-01-23 12:48:33 -0500

    Changes to sum problem

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,13 +1,26 @@
- const sumAll = function(num1, num2) {
+const sumAll = function (num1, num2) {
 
     // Create a variable to hold the final sum
     let sum = 0;
 
-    if(num1 && num2 < 0) return "ERROR";
+    // Using Number.IsInteger to check if the number is an integer
+    // Or return an ERROR
+    if ((!Number.isInteger(num1)) || (!Number.isInteger(num2))) return "ERROR";
 
-    for(let i = num1; i <= num2; i++)
-    {
-        sum = sum + i;
+    // Same goes for if the number is negative
+    if (num1 < 0 || num2 < 0) return "ERROR";
+
+    // Run an if statement to check if the number is large where num1 is start point
+    // and num2 is the end point so (2000 > 20)
+    // If so, swap it around to re-do the limits
+    if (num1 > num2) {
+        const num3 = num1;
+        num1 = num2;
+        num2 = num3;
+    }
+
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
     }
     return sum;
 };
