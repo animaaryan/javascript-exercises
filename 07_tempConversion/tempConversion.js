206539bfa98415b2eaf4b514749edb7c84742807
animaaryan <aaryan.junnarkar@gmail.com>
2025-01-23 14:13:03 -0500

    Finished temps

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,12 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function (farhenheit) {
 
-const convertToFahrenheit = function() {
-};
+  return Math.round(((farhenheit - 32) * 5 / 9) * 10) / 10;
+}
+
+const convertToFahrenheit = function (celsius) {
+
+  return Math.round(((celsius * 9 / 5) + 32) * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
